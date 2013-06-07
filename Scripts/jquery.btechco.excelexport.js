9b8bdc0e88c49d7df409f59fd0afecbe41ce2754
Tarun Batta <tarunbatta@gmail.com>
2013-06-07 15:33:15 -0700

    added test bed

--- Scripts/jquery.btechco.excelexport.js
+++ Scripts/jquery.btechco.excelexport.js
@@ -1,1 +1,35 @@
-﻿
+﻿/*
+ * jQuery Excel Export Plugin Library
+ * http://tarunbatta.blogspot.com/
+ *
+ * Copyright (c) 2013 Tarun Batta
+ * Licensed under BTechCo licenses.
+ * https://github.com/btechco/btechco_excelexport/wiki
+ *
+ */
+
+(function ($) {
+
+    $treedatatype = {
+        Table: 1
+        , Json: 2
+        , Xml: 3
+
+    }
+
+    var $defaults = {
+        containerid: null
+        , url: null
+        , async: false
+        , dataset: null
+        , datatype: $treedatatype.Table
+    };
+
+    var $settings = $defaults;
+
+    $.fn.btechco_excelexport = function (options) {
+        $settings = $.extend({}, $defaults, options);
+
+        
+    };
+})(jQuery);
