06e9f6b930369c799b5f1ea69825aa86beefa7ad
Tarun Batta <tarunbatta@gmail.com>
2014-11-25 16:39:51 -0800

    grunt implementation

---
+++ src/Gruntfile.js
@@ -0,0 +1,39 @@
+﻿module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json')
+        , uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: 'Scripts/jquery.battatech.excelexport.js',
+                dest: 'Scripts/jquery.battatech.excelexport.min.js'
+            },
+            dist: {
+                src: 'Scripts/jquery.battatech.excelexport.js',
+                dest: '../dist/jquery.battatech.excelexport.min.js'
+            }
+        }
+        , copy: {
+            main: {
+                src: 'Scripts/jquery.battatech.excelexport.js',
+                dest: '../dist/jquery.battatech.excelexport.js'
+            }
+        }
+        , watch: {
+            files: 'Scripts/jquery.battatech.excelexport.js'
+            , tasks: ['uglify']
+        }
+    });
+
+    // Load the plugin that provides the "uglify" task.
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    // Load the plugin that provides the "copy" task.
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+    // Default task(s).
+    grunt.registerTask('default', ['uglify', 'copy']);
+};
